fde99d807c58462c0b7b40ca6fb17e052d3fe0e5
Deepak-Koworkerai <165667192+Deepak-Koworkerai@users.noreply.github.com>
2024-06-10 13:56:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask, request, jsonify
 import google.generativeai as genai
+import logging
 
 app = Flask(__name__)
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def llm_model(question, data):
     model = genai.GenerativeModel('gemini-1.5-pro')
+    logger.info("-------------------------DATA PASSING TO THE MODEL!!!--------------------------")            
     response = model.generate_content(f'''You are an Friend and a AI assistant for Deepak PROVIDE THE PERFECT ANSWER IN THREE POINTS 
     for the user querstion from the available data!!\n" Question:{question} \n CONTEXT:{data}''')    
+    logger.info("-------------------------MODEL DATA DONE!!!--------------------------\n\n\n\n\n")            
     return response.text
 
 @app.route('/model', methods=['POST'])
